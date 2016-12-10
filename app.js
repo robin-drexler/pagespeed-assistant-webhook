e8ca0ffbf0165604058805896286af74e0902e0b
Robin Drexler <drexler.robin@gmail.com>
2016-12-10 19:13:15 +0100

    add psi

--- app.js
+++ app.js
@@ -4,41 +4,55 @@ process.env.DEBUG = 'actions-on-google:*';
 let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
+const psi = require('psi');
 
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
 
+
 const NAME_ACTION = 'get_pagespeed';
 const URL_ARGUMENT = 'url';
 
-app.get('/', function(req, res) {
-  console.log('GET Request headers: ' + JSON.stringify(req.headers));
-  res.send('hello world');
+app.get('/', function (req, res) {
+    psi('http://jimdo.com')
+        .then((result) => {
+            res.send('' + result.ruleGroups.SPEED.score)
+        })
+        .catch((err) => {
+            res.send(':(')
+        });
 });
 
-// [START SillyNameMaker]
+
 app.post('/', function (req, res) {
-  const assistant = new Assistant({request: req, response: res});
-  console.log('Request headers: ' + JSON.stringify(req.headers));
-  console.log('Request body: ' + JSON.stringify(req.body));
+    const assistant = new Assistant({request: req, response: res});
+    console.log('Request headers: ' + JSON.stringify(req.headers));
+    console.log('Request body: ' + JSON.stringify(req.body));
 
-  function makeName (assistant) {
-    let url = assistant.getArgument(URL_ARGUMENT);
-    assistant.tell(`Page speed for ${url} score is 34`);
-  }
+    function makeName(assistant) {
+        const url = assistant.getArgument(URL_ARGUMENT);
 
-  let actionMap = new Map();
-  actionMap.set(NAME_ACTION, makeName);
+        psi(url)
+            .then((result) => {
+                assistant.tell(`Mobile page speed for ${url} score is ${result.ruleGroups.SPEED.score}`);
+            })
+            .catch((err) => {
+                assistant.tell(`Mobile page speed for ${url} could not be determined.`);
+            });
+    }
 
-  assistant.handleRequest(actionMap);
+    let actionMap = new Map();
+    actionMap.set(NAME_ACTION, makeName);
+
+    assistant.handleRequest(actionMap);
 });
 
 if (module === require.main) {
-  // Start the server
-  let server = app.listen(process.env.PORT || 8080, function () {
-    let port = server.address().port;
-    console.log('App listening on port %s', port);
-  });
+    // Start the server
+    let server = app.listen(process.env.PORT || 8080, function () {
+        let port = server.address().port;
+        console.log('App listening on port %s', port);
+    });
 }
 
 module.exports = app;
